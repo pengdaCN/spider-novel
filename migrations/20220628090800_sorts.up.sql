81ccf038d8220117cd4a7fa9645668f3d995c075
pengda <3053787951@qq.com>
2022-06-28 17:23:38 +0800

    feat: 添加sqlx migrate 文件

--- migrations/20220628090800_sorts.up.sql
+++ migrations/20220628090800_sorts.up.sql
@@ -1,10 +1,11 @@
 -- Add up migration script here
-create table if not exists sorts (
-                                     id integer not null ,
-                                     created_at text not null ,
-                                     updated_at text,
-                                     name text not null ,
-                                     relation_kind_id text,
-                                     relation_id integer,
-                                     primary key (id)
+create table if not exists sorts
+(
+    id               integer not null,
+    created_at       text    not null,
+    updated_at       text,
+    name             text    not null,
+    relation_kind_id text,
+    relation_id      integer,
+    primary key (id)
 )
