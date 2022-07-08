2484ea5a28b65ccaf764f47c5f64d1ecdebfbdd8
pengda <3053787951@qq.com>
2022-07-08 17:27:28 +0800

    开始进行测试2

--- tests/get.rs
+++ tests/get.rs
@@ -1,10 +1,20 @@
 use skyscraper::html;
 use tokio::test;
 use spider_novel::common::httputils::get;
+use nipper::Document;
 
 #[test]
 async fn tget() {
     let x = get("http://www.ddxsku.com/").await.unwrap();
 println!("{x}");
     let _ = html::parse(&x).unwrap();
+}
+
+#[test]
+async fn tget2() {
+    let x = get("http://www.ddxsku.com/files/article/html/102/102212/39227055.html").await.unwrap();
+
+    let doc = Document::from(&x);
+    let x = doc.select("dd#contents").text();
+    println!("{}", x);
 }
