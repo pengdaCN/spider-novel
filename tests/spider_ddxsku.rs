be84ba5ff15437e56054c3c92972b9a6b66eb10e
pengda <3053787951@qq.com>
2022-07-15 23:19:49 +0800

    完善测试

--- tests/spider_ddxsku.rs
+++ tests/spider_ddxsku.rs
@@ -3,6 +3,8 @@ use spider_novel::common::snowid::set;
 use spider_novel::ddxsku::{DDSpider, SortEntity};
 use spider_novel::spider::{NovelID, Position, SortID, Spider};
 use std::sync::Arc;
+use tokio::fs::{DirBuilder, OpenOptions};
+use tokio::io::AsyncWriteExt;
 use tokio::test;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
@@ -59,4 +61,82 @@ async fn get_novels() {
     }
 
     println!("ok")
+}
+
+#[test]
+async fn get_novels2() {
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::INFO)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    set(1, 1);
+    let mut spider = ddxsku_spider().await;
+    spider.load_sorts().await.unwrap();
+
+    let id: SortID = 6953709975051046913.into();
+
+    let mut rx = spider.novels_by_sort_id(&id, Position::Full).await.unwrap();
+    loop {
+        match rx.recv().await {
+            Some(x) => {
+                println!("{:?}", x);
+            }
+            None => {
+                break;
+            }
+        }
+    }
+
+    println!("ok")
+}
+
+#[test]
+async fn test_sections2() {
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::INFO)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+    set(1, 1);
+    let mut spider = ddxsku_spider().await;
+    spider.load_sorts().await.unwrap();
+
+    let id: NovelID = 6953718276044230657.into();
+
+    let mut tx = spider.sections_by_novel_id(&id, Position::Full).await.unwrap();
+    let novel_name = "大唐全能奶爸";
+    let path = &format!("/tmp/{novel_name}");
+    DirBuilder::new().recursive(true).create(path).await.unwrap();
+
+    loop {
+        match tx.recv().await {
+            Some(x) => {
+                match x {
+                    Ok(section) => {
+                        let section_path = format!("{path}/{}-{}", section.seq, section.name);
+
+                        let mut f = OpenOptions::new().create(true).write(true).open(&section_path).await.unwrap();
+                        f.write_all(&section.text.as_bytes()).await.unwrap();
+
+                        f.flush().await.unwrap();
+                    }
+                    Err(e) => {
+                        println!("获取章节错误：{e}");
+                    }
+                }
+            }
+            None => break,
+        }
+    }
 }
