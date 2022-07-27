044e2da8da62667a3f01cc51d96060ce96dd4477
pengda <3053787951@qq.com>
2022-07-27 23:32:36 +0800

    添加quickjs

--- tests/quickjs.rs
+++ tests/quickjs.rs
@@ -5,5 +5,13 @@ fn js_eval() {
     let context = Context::new().unwrap();
 
     let x = context.eval_as::<i32>("10 + 30").unwrap();
-    println!("{x}")
+    println!("{x}");
+
+    context.set_global("age", 10).unwrap();
+
+    let x = context.eval_as::<i32>("age + 1000").unwrap();
+    println!("{x}");
+
+    let x = context.eval_as::<String>(r#"JSON.stringify({name:"pengda"})"#).unwrap();
+    println!("{x}");
 }
