bd9fcce9b397592a5845d68a2868606a3ae56cdc
pengda <3053787951@qq.com>
2022-07-29 17:41:03 +0800

    添加webook模块

--- src/webook.rs
+++ src/webook.rs
@@ -35,7 +35,7 @@ pub struct BookSource {
     /// 注释
     comment: Option<String>,
     /// 自定义变量说明
-    variable_comment: Option<String>,
+    iable_comment: Option<String>,
     /// 发现url
     explore_url: Option<String>,
     /// 发现规则
@@ -52,12 +52,70 @@ pub struct BookSource {
     rule_content: Option<ContentRule>,
 }
 
-pub struct ExploreRule {}
+pub struct ExploreRule {
+    book_list: Option<String>,
+    name: Option<String>,
+    author: Option<String>,
+    intro: Option<String>,
+    kind: Option<String>,
+    last_chapter: Option<String>,
+    update_time: Option<String>,
+    book_url: Option<String>,
+    cover_url: Option<String>,
+    word_count: Option<String>,
+}
 
-pub struct SearchRule {}
+pub struct SearchRule {
+    /// 校验关键字
+    check_keyword: Option<String>,
+    book_list: Option<String>,
+    name: Option<String>,
+    author: Option<String>,
+    intro: Option<String>,
+    kind: Option<String>,
+    last_chapter: Option<String>,
+    update_time: Option<String>,
+    book_url: Option<String>,
+    cover_url: Option<String>,
+    word_count: Option<String>,
+}
 
-pub struct InfoRule {}
+pub struct InfoRule {
+    init: Option<String>,
+    name: Option<String>,
+    author: Option<String>,
+    intro: Option<String>,
+    kind: Option<String>,
+    last_chapter: Option<String>,
+    update_time: Option<String>,
+    cover_url: Option<String>,
+    toc_url: Option<String>,
+    word_count: Option<String>,
+    can_re_name: Option<String>,
+    download_urls: Option<String>,
+}
 
-pub struct TocRule {}
+pub struct TocRule {
+    pre_update_js: Option<String>,
+    chapter_list: Option<String>,
+    chapter_name: Option<String>,
+    chapter_url: Option<String>,
+    is_volume: Option<String>,
+    is_vip: Option<String>,
+    is_pay: Option<String>,
+    update_time: Option<String>,
+    next_toc_url: Option<String>,
+}
 
-pub struct ContentRule {}
+pub struct ContentRule {
+    content: Option<String>,
+    next_content_url: Option<String>,
+    web_js: Option<String>,
+    source_regex: Option<String>,
+    /// 替换规则
+    replace_regex: Option<String>,
+    /// 默认大小居中,FULL最大宽度
+    image_style: Option<String>,
+    /// 购买操作,js或者包含{{js}}的url
+    pay_action: Option<String>,
+}
